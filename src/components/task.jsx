345008ddad49dcec05ed1e3d9dacc0a673fbe041
Sarvagya196 <sarvagya1710.com>
2024-06-28 15:29:28 +0530

    responsive

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -35,9 +35,9 @@ function Task( {mId, isCompleted=false, tit=""}){
 
     return (
         <>
-            <div className="relative z-1 w-[300px] min-w-[300px] flex flex-col gap-[10px] bg-black text-white rounded-lg">
+            <div className="relative z-1 w-[300px] max-[639px]:min-w-full min-w-[300px] flex flex-col gap-[10px] bg-black text-white rounded-lg">
                 <div className="flex gap-[10px] m-[10px]">
-                    <input type="text" value={title} placeholder="Enter Task Here" onChange={handleChange} className={`bg-transparent h-auto resize-none ${isCompleted === true ? 'line-through' : 'no-underline'} placeholder:opacity-40`}/>
+                    <input type="text" value={title} placeholder="Enter Task Here" onChange={handleChange} className={`bg-transparent max-[639px]:w-[80vw] h-auto resize-none ${isCompleted === true ? 'line-through' : 'no-underline'} placeholder:opacity-40`}/>
                     <button
                     onClick={handleDelete}>
                         <img alt="delete" src={deleteIcon} className="h-[20px] opacity-50"/>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,12 +15,12 @@ function Home(){
     return (
         <>
         <div className="relative z-1 p-[10px] flex gap-[10px] bg-[#9e5173] h-[100vh] w-full">
-            <div className="flex gap-[10px] mr-[310px] overflow-hidden hover:overflow-x-auto">
+            <div className="flex gap-[10px] mr-[310px] max-[639px]:mr-0 max-[639px]:mb-[60px] sm:max-lg:mr-[210px] overflow-hidden hover:overflow-x-auto">
             {maintodos?.map((mtodo) => (
                 <Task key={mtodo?.id} mId={mtodo.id} isCompleted={mtodo.completed} tit={mtodo.title}/>
             ))}
             </div>
-            <div className="absolute z-2 right-[10px] h-[50px] w-[300px] flex gap-[10px] items-center text-white bg-white/30 rounded-lg">
+            <div className="absolute z-2 right-[10px] max-[639px]:right-0 max-[639px]:bottom-[10px] h-[50px] w-[300px] sm:max-lg:w-[200px] max-[639px]:w-full flex gap-[10px] items-center text-white bg-white/30 rounded-lg">
                 <img
                 alt="Add card" src={plus} className="h-[40px]"
                 onClick={handleAdd}></img>
